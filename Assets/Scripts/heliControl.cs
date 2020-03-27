05b7db16007e4bb8b1c0a241fb041bc17e29c307
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-03-28 00:42:33 +0300

    set speed animation.

--- Assets/Scripts/heliControl.cs
+++ Assets/Scripts/heliControl.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class heliControl : MonoBehaviour
 {
+    Rigidbody helicopterRigid;
     public Sprite[] helicopterSprite;
     SpriteRenderer spriteRenderer;
     float _time = 0;
-        float timeLimit = 0.1F; //gaz verildiğin timeLimit 0,04 olmalı
+    float timeLimit = 0.1F; //gaz verildiğin timeLimit 0,04 olmalı
+    float vertical = 0;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        helicopterRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       
+        Debug.Log(vertical);
+        vertical = Input.GetAxisRaw("Vertical");
+        Debug.Log(vertical);
+        if (vertical > 0)
+        {
+            timeLimit = 0.07F;
+        }
+        else
+        {
+            timeLimit = 0.1F;
+        }
         _time += Time.deltaTime;
         if(_time > timeLimit && spriteRenderer.sprite == helicopterSprite[0])
         {
