bab2f55761a5ae7cf9432e5d3748683fb8d44731
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-03-28 01:12:18 +0300

    added basic controls.

--- Assets/Scripts/heliControl.cs
+++ Assets/Scripts/heliControl.cs
@@ -10,19 +10,19 @@ public class heliControl : MonoBehaviour
     float _time = 0;
     float timeLimit = 0.1F;
     float vertical = 0;
+    public int speed = 10;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         helicopterRigid = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        Debug.Log(vertical);
         vertical = Input.GetAxisRaw("Vertical");
-        Debug.Log(vertical);
         if (vertical > 0)
         {
             timeLimit = 0.07F;
@@ -38,15 +38,19 @@ public class heliControl : MonoBehaviour
         {
             spriteRenderer.sprite = helicopterSprite[1];
             _time = 0;
-            Debug.Log("0");
         }
         else if (_time > timeLimit && spriteRenderer.sprite == helicopterSprite[1])
         {
             spriteRenderer.sprite = helicopterSprite[0];
             _time = 0;
-            Debug.Log("1");
+     
         }
         
 
+    }
+    void FixedUpdate()
+    {
+        Vector3 vec = new Vector3(0, vertical, 0);
+        helicopterRigid.AddForce(vec * speed);
     }
 }
