2f32c03d432cf5d96986d138ad1697a59a2f5319
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-07 20:19:06 +0300

    changed background and blocks.

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -7,19 +7,36 @@ public class GameControl : MonoBehaviour
     // Start is called before the first frame update
     public GameObject background1;
     public GameObject background2;
+    public GameObject block;
+    public int blockNumber;
+    public float backgroundSpeed = -2f;
+    private GameObject[] blocks;
+
     Rigidbody2D rigid1;
     Rigidbody2D rigid2;
+    Rigidbody2D blockRigid;
+
+    float t = 0;
+    int counter = 0;
     private float size = 0;
     void Start()
     {
         rigid1 = background1.GetComponent<Rigidbody2D>();
         rigid2 = background2.GetComponent<Rigidbody2D>();
-
-        rigid1.velocity = new Vector2(-1.5f, 0);
-        rigid2.velocity = new Vector2(-1.5f, 0);
-
+        
+        rigid1.velocity = new Vector2(-backgroundSpeed, 0);
+        rigid2.velocity = new Vector2(-backgroundSpeed, 0);
+        
         size = background1.GetComponent<BoxCollider2D>().size.x;
-        Debug.Log(size);
+        blocks = new GameObject[blockNumber];
+         
+        for (int i= 0; i < blocks.Length; i++)
+        {
+            blocks[i] = Instantiate(block, new Vector2(-20, -20), Quaternion.identity);
+            blockRigid = blocks[i].AddComponent<Rigidbody2D>();
+            blockRigid.gravityScale = 0;
+            blockRigid.velocity = new Vector2(-backgroundSpeed, 0);
+        }
     }
 
     // Update is called once per frame
@@ -32,6 +49,18 @@ public class GameControl : MonoBehaviour
         if (background2.transform.position.x <= -size)
         {
             background2.transform.position += new Vector3(size * 2, 0);
+        }
+        t = Time.deltaTime;
+        if (t > 2f)
+        {
+            t = 0;
+            float yAxis = Random.Range(-0.48f, 1.71f);
+            blocks[counter].transform.position = new Vector3(6, yAxis);
+            counter++;
+            if (counter >= blocks.Length)
+            {
+                counter = 0;
+            }
         }
     }
 }
