9e367680b30813b2d4e43bfed8e4c921a557fb51
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-28 15:46:04 +0300

    created Strings.cs

---
+++ Assets/Scripts/Strings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings : MonoBehaviour
+{
+    public static string highScoreEng ="High Score:";
+    public static string gameOverEng = "Game Over";
+    public static string clickToStartEng = "Click to Start";
+    public static string highScoreTur="En Yüksek Puan";
+    public static string gameOverTur="Oyun Bitti";
+    public static string clickToStartTur="Başlamak için Tıklayınız";
+}
