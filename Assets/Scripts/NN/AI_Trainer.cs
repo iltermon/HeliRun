61371abb3d5b90d23a4a01d061e659820307a803
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-02 17:22:06 +0300

    changed raycast from 3d to 2d

--- Assets/Scripts/NN/AI_Trainer.cs
+++ Assets/Scripts/NN/AI_Trainer.cs
@@ -10,15 +10,16 @@ public class AI_Trainer : MonoBehaviour
 
 	AI_DriveController driveController;
 	Transform raycastPoint;
-	RaycastHit[] sensors;
+	RaycastHit2D[] sensors;
 	GameObject environments;
 	GameObject[] lines;
     public Material lineRendererMaterial;
-	GameObject cave1;
-	GameObject cave2;
+	public GameObject cave1;
+	public GameObject cave2;
+	
 	Vector3 startPosition;
 	Quaternion startRotation;
-
+	
 	Vector3 currCarPos;
 	Vector3 lastCarPos;
 	public float totalDist;
@@ -26,20 +27,17 @@ public class AI_Trainer : MonoBehaviour
 
 	public float timeScale = 1f;
 
-	float rayDist = 11;
+	float rayDist = 10f;
 
 	// Use this for initialization
 	void Start ()
 	{
-		population = new Population(10, new int[]{5, 200, 2}, 1f);
 
+		population = new Population(10, new int[]{4, 200, 1}, 1f);
 		raycastPoint = transform.Find("RaycastPoint");	
-
 		startPosition = transform.position;
 		startRotation = transform.rotation;
-
 		currCarPos = lastCarPos = startPosition;
-
 		currNN = population.Next();
 	}
 
@@ -63,50 +61,59 @@ public class AI_Trainer : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+
 		Time.timeScale = timeScale;
 
-		sensors = new RaycastHit[3];
+		sensors = new RaycastHit2D[3];
 
-		Physics.Raycast(raycastPoint.position, raycastPoint.up, out sensors[0], rayDist);
-		Physics.Raycast(raycastPoint.position, raycastPoint.forward, out sensors[1], rayDist);
-		Physics.Raycast(raycastPoint.position, -raycastPoint.up, out sensors[2], rayDist);
-
-        DrawSensorLines();
+		sensors[0] = Physics2D.Raycast(raycastPoint.position, raycastPoint.up,rayDist);
+		sensors[1] = Physics2D.Raycast(raycastPoint.position, raycastPoint.forward, rayDist);
+		sensors[2] = Physics2D.Raycast(raycastPoint.position, -raycastPoint.up, rayDist);
+		Debug.DrawLine(raycastPoint.position, sensors[2].point);
+		Debug.DrawLine(raycastPoint.position, sensors[0].point);
+		Debug.DrawLine(raycastPoint.position, sensors[1].point);
 
 		float forward, up, down;
 		forward = down = up = rayDist;
-
-		if(sensors[0].collider !=cave1.GetComponent<BoxCollider2D>() || sensors[0].collider !=cave2.GetComponent<BoxCollider2D>())
+		if (sensors[0].collider != cave1.GetComponent<BoxCollider2D>() || sensors[0].collider != cave2.GetComponent<BoxCollider2D>() || !sensors[1].collider.CompareTag("point")) 
+		{ 
 			up = sensors[0].distance;
 
-		if(sensors[1].collider !=cave1.GetComponent<BoxCollider2D>() || sensors[1].collider !=cave2.GetComponent<BoxCollider2D>())
-			forward = sensors[1].distance;
+		}
 
-		if(sensors[2].collider !=cave1.GetComponent<BoxCollider2D>() || sensors[2].collider !=cave2.GetComponent<BoxCollider2D>())
+		if (sensors[1].collider != cave1.GetComponent<BoxCollider2D>() || sensors[1].collider != cave2.GetComponent<BoxCollider2D>() || !sensors[1].collider.CompareTag("point"))
+		{ 
+			forward = sensors[1].distance;
+			sensors[1].collider.tag.ToString();
+			Debug.Log(sensors[1].collider.tag.ToString());
+		}
+
+		if (sensors[2].collider != cave1.GetComponent<BoxCollider2D>() || sensors[2].collider != cave2.GetComponent<BoxCollider2D>() || !sensors[1].collider.CompareTag("point"))
+		{ 
 			down = sensors[2].distance;
 
-        double[] inputs = new double[5];
-		inputs[0] = (2f / rayDist) * forward - 1f;
-		inputs[1] = (2f / rayDist) * left - 1f;
-		inputs[2] = (2f / rayDist) * right - 1f;
-        inputs[3] = (2f / rayDist) * leftMid - 1f;
-        inputs[4] = (2f / rayDist) * rightMid - 1f;
+		}
 
-		double[] outputs;
-		outputs = currNN.FeedForward(inputs);
+		currNN.input.matrix[0, 0] = (2f / rayDist) * up - 1f;
+		currNN.input.matrix[1, 0] = (2f / rayDist) * forward - 1f;
+		currNN.input.matrix[2, 0] = (2f / rayDist) * down - 1f;
+		currNN.input.matrix[3, 0] = GetComponent<Rigidbody2D>().velocity[1];
+		currNN.FeedForward();
 
-		helicontrol.vertical=currNN.output[0,0];
+
+		//heliControl.vertical = currNN.output.matrix[0, 0];
 
 		currCarPos = transform.position;
 		totalDist += Vector3.Distance(currCarPos, lastCarPos);
 		lastCarPos = currCarPos;
 
 		timePassed += Time.deltaTime;
+
 	}
 
 	void OnCollisionEnter()
 	{
-		population.SetFitnessOfCurrIndividual(totalDist, timePassed);
+		//population.SetFitnessOfCurrIndividual(totalDist, timePassed);
 		currNN = population.Next();
 		ResetCarPosition();
 	}
@@ -124,92 +131,91 @@ public class AI_Trainer : MonoBehaviour
 		timePassed = 0f;
 	}
 
-	void DrawSensorLines()
-	{
-		Color middleSensorColor, leftSensorColor, rightSensorColor, leftMiddleSensorColor, rightMiddleSensorColor;
-		middleSensorColor = (sensors[0].collider == null) ? Color.green : Color.red;
-		leftSensorColor   = (sensors[1].collider == null) ? Color.green : Color.red;
-		rightSensorColor  = (sensors[2].collider == null) ? Color.green : Color.red;
-        leftMiddleSensorColor = (sensors[3].collider == null) ? Color.green : Color.red;
-        rightMiddleSensorColor = (sensors[4].collider == null) ? Color.green : Color.red;
+	//void DrawSensorLines()
+	//{
+	//	Color middleSensorColor, leftSensorColor, rightSensorColor, leftMiddleSensorColor, rightMiddleSensorColor;
+	//	middleSensorColor = (sensors[0].collider == null) ? Color.green : Color.red;
+	//	leftSensorColor   = (sensors[1].collider == null) ? Color.green : Color.red;
+	//	rightSensorColor  = (sensors[2].collider == null) ? Color.green : Color.red;
 
-        if (lines == null)
-		{
-			lines = new GameObject[5];
-			DrawLine(
-				raycastPoint.position,
-				(raycastPoint.position + raycastPoint.forward * rayDist), 
-				middleSensorColor,
-				0
-			);
 
-			DrawLine(
-				raycastPoint.position,
-				(raycastPoint.position + (raycastPoint.forward - raycastPoint.right) * rayDist),
-				leftMiddleSensorColor,
-				1
-			);
+  //      if (lines == null)
+		//{
+		//	lines = new GameObject[5];
+		//	DrawLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + raycastPoint.forward * rayDist), 
+		//		middleSensorColor,
+		//		0
+		//	);
 
-			DrawLine(
-				raycastPoint.position,
-				(raycastPoint.position + (raycastPoint.forward + raycastPoint.right) * rayDist),
-				rightMiddleSensorColor,
-				2
-			);
+		//	DrawLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + (raycastPoint.forward - raycastPoint.right) * rayDist),
+		//		leftMiddleSensorColor,
+		//		1
+		//	);
 
-            DrawLine(
-                raycastPoint.position,
-                (raycastPoint.position + (-raycastPoint.right) * rayDist),
-                leftMiddleSensorColor,
-                3
-            );
+		//	DrawLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + (raycastPoint.forward + raycastPoint.right) * rayDist),
+		//		rightMiddleSensorColor,
+		//		2
+		//	);
 
-            DrawLine(
-                raycastPoint.position,
-                (raycastPoint.position + raycastPoint.right * rayDist),
-                rightMiddleSensorColor,
-                4
-            );
-        }
-		else
-		{
-			UpdateLine(
-				raycastPoint.position,
-				(raycastPoint.position + raycastPoint.forward * rayDist), 
-				middleSensorColor,
-				0
-			);
+  //          DrawLine(
+  //              raycastPoint.position,
+  //              (raycastPoint.position + (-raycastPoint.right) * rayDist),
+  //              leftMiddleSensorColor,
+  //              3
+  //          );
 
-			UpdateLine(
-				raycastPoint.position,
-				(raycastPoint.position + (raycastPoint.forward - raycastPoint.right).normalized * rayDist),
-				leftMiddleSensorColor,
-				1
-			);
+  //          DrawLine(
+  //              raycastPoint.position,
+  //              (raycastPoint.position + raycastPoint.right * rayDist),
+  //              rightMiddleSensorColor,
+  //              4
+  //          );
+  //      }
+		//else
+		//{
+		//	UpdateLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + raycastPoint.forward * rayDist), 
+		//		middleSensorColor,
+		//		0
+		//	);
 
-			UpdateLine(
-				raycastPoint.position,
-				(raycastPoint.position + (raycastPoint.forward + raycastPoint.right).normalized * rayDist),
-				rightMiddleSensorColor,
-				2
-			);
+		//	UpdateLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + (raycastPoint.forward - raycastPoint.right).normalized * rayDist),
+		//		leftMiddleSensorColor,
+		//		1
+		//	);
 
-            UpdateLine(
-                raycastPoint.position,
-                (raycastPoint.position + (-raycastPoint.right).normalized * rayDist),
-                leftSensorColor,
-                3
-            );
+		//	UpdateLine(
+		//		raycastPoint.position,
+		//		(raycastPoint.position + (raycastPoint.forward + raycastPoint.right).normalized * rayDist),
+		//		rightMiddleSensorColor,
+		//		2
+		//	);
 
-            UpdateLine(
-                raycastPoint.position,
-                (raycastPoint.position + raycastPoint.right.normalized * rayDist),
-                rightSensorColor,
-                4
-            );
-        }
+  //          UpdateLine(
+  //              raycastPoint.position,
+  //              (raycastPoint.position + (-raycastPoint.right).normalized * rayDist),
+  //              leftSensorColor,
+  //              3
+  //          );
 
-	}
+  //          UpdateLine(
+  //              raycastPoint.position,
+  //              (raycastPoint.position + raycastPoint.right.normalized * rayDist),
+  //              rightSensorColor,
+  //              4
+  //          );
+        //}
+
+	//}
 		
 	void DrawLine(Vector3 start, Vector3 end, Color color, int lineIndex)
 	{
