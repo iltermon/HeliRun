2c21e97297f78e22f107fa52b9ba586d1a6b5915
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-08 17:01:21 +0300

    added new sensors

--- Assets/Scripts/NN/AI_Trainer.cs
+++ Assets/Scripts/NN/AI_Trainer.cs
@@ -10,19 +10,24 @@ public class AI_Trainer : MonoBehaviour
 	Transform raycastPoint;
 	RaycastHit2D[] sensors;
     public Material lineRendererMaterial;
-	Vector3 startPosition;
-	Vector3 startPosCave1;
-	Vector3 startPosCave2;
-	Vector3 currCarPos;
-	Vector3 lastCarPos;
+	Vector2 startPosition;
+	Transform topSensorPos;
+	Transform bottomSensorPos;
+	Vector2 startPosCave1;
+	Vector2 startPosCave2;
+	Vector2 currCarPos;
+	Vector2 lastCarPos;
 	public int maxDist;
 	public float timePassed;
 	public float timeScale = 1f;
     readonly float rayDist = 8;
 	public int dist=0;
 	private static AI_Trainer instance;
-	public float right, up, down, vel;
-	public static AI_Trainer Instance { get { return instance; } }
+	public float topfwd, botfwd, right, up, down, vel;
+    private float maxVel;
+    private float minVel;
+
+    public static AI_Trainer Instance { get { return instance; } }
 	
 	
 	private void Awake()
@@ -40,8 +45,10 @@ public class AI_Trainer : MonoBehaviour
 	void Start ()
 	{
 		dist=0;
-		population = new Population(10, new int[]{3, 25, 1}, 1f);
-		raycastPoint = transform.Find("RaycastPoint");	
+		population = new Population(10, new int[]{6, 150, 1}, 1f,30);
+		raycastPoint = transform.Find("RaycastPoint2");	
+		topSensorPos = transform.Find("RaycastPoint1");	
+		bottomSensorPos = transform.Find("RaycastPoint3");	
 		startPosition = transform.position;
 		currCarPos = lastCarPos = startPosition;
 		currNN = population.Next();
@@ -68,15 +75,18 @@ public class AI_Trainer : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		
 		dist++;
 		AI_UI.Instance.distanceText.text = dist.ToString();
-		
 		Time.timeScale = timeScale;
-		sensors = new RaycastHit2D[3];
+		sensors = new RaycastHit2D[5];
+		//daha fazla sensör ekle
 		sensors[0] = Physics2D.Raycast(raycastPoint.position, raycastPoint.up, rayDist);
 		sensors[1] = Physics2D.Raycast(raycastPoint.position, raycastPoint.right, rayDist);
 		sensors[2] = Physics2D.Raycast(raycastPoint.position, -raycastPoint.up, rayDist);
-		right = down = up = rayDist;
+		sensors[3] = Physics2D.Raycast(topSensorPos.position, topSensorPos.right, rayDist);
+		sensors[4] = Physics2D.Raycast(bottomSensorPos.position, bottomSensorPos.right, rayDist);
+		right = topfwd = botfwd = down = up = rayDist;
 			if (sensors[0].collider != null )
 			{
 				if(!sensors[0].collider.CompareTag("point"))
@@ -99,22 +109,48 @@ public class AI_Trainer : MonoBehaviour
 				down = sensors[2].distance;
 				}
 			}
+			if (sensors[3].collider !=null)
+			{
+				if(!sensors[3].collider.CompareTag("point"))
+				{
+				topfwd = sensors[3].distance;
+				}
+			}
+			if (sensors[4].collider !=null)
+			{
+				if(!sensors[4].collider.CompareTag("point"))
+				{
+				botfwd = sensors[4].distance;
+				}
+			}
+		maxVel=14;
+		minVel=-17f;
 		vel = GetComponent<Rigidbody2D>().velocity[1];
+		if(vel<0)
+		{
+			
+		}
 		currNN.input.matrix[0, 0] = (2f / rayDist) * up - 1f;
 		currNN.input.matrix[1, 0] = (2f / rayDist) * right - 1f;
 		currNN.input.matrix[2, 0] = (2f / rayDist) * down - 1f;
-		currNN.FeedForward();
-
-		heliControl.Instance.vertical = currNN.output.matrix[0, 0];
+		currNN.input.matrix[3, 0] = 2f*((vel-minVel)/(maxVel-minVel))-1;
+		currNN.input.matrix[4, 0] = (2f / rayDist) * topfwd - 1f;
+		currNN.input.matrix[5, 0] = (2f / rayDist) * botfwd - 1f;
+		currNN.FeedForward(); 
+		if(currNN.output.matrix[0, 0]>0)
+		{
+		heliControl.Instance.Accelerate();
+		}
+		else
+		{
+			heliControl.Instance.vertical=0;
+		}
 
 		currCarPos = transform.position;
-		
 		lastCarPos = currCarPos;
-
 		timePassed += Time.deltaTime;
 
 	}
-
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (!collision.CompareTag("point"))
@@ -126,7 +162,7 @@ public class AI_Trainer : MonoBehaviour
 		}
 	}
 
-	void ResetCarPosition()
+	public void ResetCarPosition()
 	{
 		transform.position = startPosition;
 		currCarPos = startPosition;
@@ -140,119 +176,4 @@ public class AI_Trainer : MonoBehaviour
 		dist=0;
 		
 	}
-
-// 	void DrawSensorLines()
-// 	{
-// 		Color middleSensorColor, leftSensorColor, rightSensorColor, leftMiddleSensorColor, rightMiddleSensorColor;
-// 		middleSensorColor = (sensors[0].collider == null) ? Color.green : Color.red;
-// 		leftSensorColor   = (sensors[1].collider == null) ? Color.green : Color.red;
-// 		rightSensorColor  = (sensors[2].collider == null) ? Color.green : Color.red;
-
-
-//        if (lines == null)
-// 		{
-// 			lines = new GameObject[5];
-// 			DrawLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + raycastPoint.forward * rayDist), 
-// 				middleSensorColor,
-// 				0
-// 			);
-
-// 			DrawLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + (raycastPoint.forward - raycastPoint.right) * rayDist),
-// 				leftMiddleSensorColor,
-// 				1
-// 			);
-
-// 			DrawLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + (raycastPoint.forward + raycastPoint.right) * rayDist),
-// 				rightMiddleSensorColor,
-// 				2
-// 			);
-
-//            DrawLine(
-//                raycastPoint.position,
-//                (raycastPoint.position + (-raycastPoint.right) * rayDist),
-//                leftMiddleSensorColor,
-//                3
-//            );
-
-//            DrawLine(
-//                raycastPoint.position,
-//                (raycastPoint.position + raycastPoint.right * rayDist),
-//                rightMiddleSensorColor,
-//                4
-//            );
-//        }
-// 		else
-// 		{
-// 			UpdateLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + raycastPoint.forward * rayDist), 
-// 				middleSensorColor,
-// 				0
-// 			);
-
-// 			UpdateLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + (raycastPoint.forward - raycastPoint.right).normalized * rayDist),
-// 				leftMiddleSensorColor,
-// 				1
-// 			);
-
-// 			UpdateLine(
-// 				raycastPoint.position,
-// 				(raycastPoint.position + (raycastPoint.forward + raycastPoint.right).normalized * rayDist),
-// 				rightMiddleSensorColor,
-// 				2
-// 			);
-
-//            UpdateLine(
-//                raycastPoint.position,
-//                (raycastPoint.position + (-raycastPoint.right).normalized * rayDist),
-//                leftSensorColor,
-//                3
-//            );
-
-//            UpdateLine(
-//                raycastPoint.position,
-//                (raycastPoint.position + raycastPoint.right.normalized * rayDist),
-//                rightSensorColor,
-//                4
-//            );
-//         }
-
-// 	}
-		
-// 	void DrawLine(Vector3 start, Vector3 end, Color color, int lineIndex)
-// 	{
-// 		GameObject line = new GameObject();
-// 		line.name = "Line " + lineIndex;
-// 		line.transform.SetParent(environments.transform);
-
-// 		line.transform.position = start;
-// 		line.AddComponent<LineRenderer>();
-// 		LineRenderer lr = line.GetComponent<LineRenderer>();
-//        lr.material = lineRendererMaterial; //new Material(Shader.Find("Particles/Priority Alpha Blended"));
-// 		lr.startColor = color;
-// 		lr.endColor = color;
-// 		lr.startWidth = 0.05f;
-// 		lr.endWidth = 0.05f;
-// 		lr.SetPosition(0, start);
-// 		lr.SetPosition(1, end);
-
-// 		lines[lineIndex] = line;
-// 	}
-
-// 	void UpdateLine(Vector3 start, Vector3 end, Color color, int lineIndex)
-// 	{
-// 		LineRenderer lr = lines[lineIndex].GetComponent<LineRenderer>();
-// 		lr.startColor = color;
-// 		lr.endColor = color;
-// 		lr.SetPosition(0, start);
-// 		lr.SetPosition(1, end);
-// 	}
  }
