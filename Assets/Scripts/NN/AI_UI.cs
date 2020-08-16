048b01591c95663470d56329ab9775d2dc0af184
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-08-16 11:19:53 +0300

    Update AI_UI.cs

--- Assets/Scripts/NN/AI_UI.cs
+++ Assets/Scripts/NN/AI_UI.cs
@@ -36,14 +36,14 @@ public class AI_UI : MonoBehaviour
     void Update ()
 	{
 		Debug.Log(AI_Trainer.Instance.currNN.input.matrix[3,0].ToString());
-		distanceText.text=AI_Trainer.Instance.dist.ToString();
-		upText.text=AI_Trainer.Instance.currNN.input.matrix[0, 0].ToString();
-		downText.text=AI_Trainer.Instance.currNN.input.matrix[2, 0].ToString();
-		rightText.text=AI_Trainer.Instance.currNN.input.matrix[1, 0].ToString();
-		indivText.text=AI_Trainer.Instance.population.generationNumber.ToString();
-		populationText.text=AI_Trainer.Instance.population.currIndiv.ToString();
-		maxText.text = AI_Trainer.Instance.maxDist.ToString();
-		topText.text= AI_Trainer.Instance.currNN.input.matrix[4, 0].ToString();
-		botText.text = AI_Trainer.Instance.currNN.input.matrix[5, 0].ToString();
+		distanceText.text="Dist: " + AI_Trainer.Instance.dist.ToString();
+		upText.text="Up: " + AI_Trainer.Instance.currNN.input.matrix[0, 0].ToString();
+		downText.text="Down: " + AI_Trainer.Instance.currNN.input.matrix[2, 0].ToString();
+		rightText.text="Right: " + AI_Trainer.Instance.currNN.input.matrix[1, 0].ToString();
+		indivText.text="Popu: " + AI_Trainer.Instance.population.generationNumber.ToString();
+		populationText.text="Indiv: " + AI_Trainer.Instance.population.currIndiv.ToString();
+		maxText.text = "MDist: " + AI_Trainer.Instance.maxDist.ToString();
+		topText.text= "TRigh: " + AI_Trainer.Instance.currNN.input.matrix[4, 0].ToString();
+		botText.text = "BRight: " + AI_Trainer.Instance.currNN.input.matrix[5, 0].ToString();
 	}
 }
